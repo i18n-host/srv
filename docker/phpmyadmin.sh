3f03c17263fc1715d57857e8b1b850119a1fc2dd
i18n.host <i18n.host@gmail.com>
2025-06-21 17:01:41 +0800

    ♨

--- docker/phpmyadmin.sh
+++ docker/phpmyadmin.sh
@@ -5,9 +5,9 @@ cd $DIR
 set -ex
 
 PORT=9232
-docker run -d --name myadmin \
+docker run -d --rm --name myadmin \
   --add-host=host.docker.internal:host-gateway \
-  -v ../conf/phpmyadmin.config.php:/etc/phpmyadmin/config.user.inc.php \
+  -v ${DIR%/*}/conf/phpmyadmin.config.php:/etc/phpmyadmin/config.user.inc.php \
   -v ./mnt/cache/phpmyadmin/session/:/sessions/ \
   -p $PORT:80 phpmyadmin/phpmyadmin &&
   sleep 1 && open http://localhost:$PORT
