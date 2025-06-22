0989c6a6c516205e0c5d65629a0141ff56a247e1
i18n.host <i18n.host@gmail.com>
2025-06-22 18:45:51 +0800

    ♨

--- mod/gen/_hpc.rs
+++ mod/gen/_hpc.rs
@@ -1,6 +1,5 @@
 //! AUTO GEN BY rust2proto , DO NOT EDIT
 
-use aok::anyhow;
 use dstr::dvec;
 use hpc::{call_err, args_decode};
 use ih::{CodeBody,State};
@@ -25,7 +24,7 @@ Func::None => (State::OK, vec![]),
 Func::CaptchaVerify => {
   let args: pb::captcha::VerifyArgs = args_decode(args,"captcha::Verify")?;
   match captcha::verify(&args.id,&args.click_pos_li).await {
-    Err(err)=>call_err("captcha::verify", err, captcha, || dvec![&args.id,&args.click_pos_li].join(",")).await?,
+Err(err)=>call_err("captcha::verify", err, captcha, || dvec![&args.id,&args.click_pos_li].join(",")).await?,
     Ok(r)=>(State::OK, pb::captcha::String {
     v: r.into()
   }.serialize_to_vec())
@@ -34,7 +33,7 @@ Func::CaptchaVerify => {
 
 Func::Captcha => {
   match captcha::captcha().await {
-    Err(err)=>call_err("captcha::captcha", err, captcha, || s_::EMPTY).await?,
+Err(err)=>call_err("captcha::captcha", err, captcha, || s_::EMPTY).await?,
     Ok(r)=>(State::OK, pb::captcha::Captcha {
     id: r.id,
     img: r.img,
@@ -47,7 +46,7 @@ Func::Captcha => {
 Func::UserName => {
   let user = ctx_::get(ctx).await?;
   match user::name(&user).await {
-    Err(err)=>call_err("user::name", err, captcha, || dvec![&user].join(",")).await?,
+Err(err)=>call_err("user::name", err, captcha, || dvec![&user].join(",")).await?,
     Ok(r)=>(State::OK, pb::user::String {
     v: r.into()
   }.serialize_to_vec())
@@ -62,7 +61,7 @@ Func::AuthSigninMail => {
   let set_header = ctx_::sync::get(ctx) ;
   let browser = ctx.req.extensions.get().unwrap();
   match auth::signin::mail(&args.address,&args.password,&headers,&set_header,&browser).await {
-    Err(err)=>call_err("auth::signin::mail", err, captcha, || dvec![&args.address,&args.password,&headers,&set_header,&browser].join(",")).await?,
+Err(err)=>call_err("auth::signin::mail", err, captcha, || dvec![&args.address,&args.password,&headers,&set_header,&browser].join(",")).await?,
     Ok(_)=>(State::OK, vec![])
   }
 }
@@ -72,7 +71,7 @@ Func::AuthSignupMail => {
   let args: pb::auth::SignupMailArgs = args_decode(args,"auth::SignupMail")?;
   let headers = &ctx.req.headers;
   match auth::signup::mail(&args.address,&args.password,&headers).await {
-    Err(err)=>call_err("auth::signup::mail", err, captcha, || dvec![&args.address,&args.password,&headers].join(",")).await?,
+Err(err)=>call_err("auth::signup::mail", err, captcha, || dvec![&args.address,&args.password,&headers].join(",")).await?,
     Ok(_)=>(State::OK, vec![])
   }
 }
@@ -80,18 +79,15 @@ Func::AuthSignupMail => {
 Func::AuthTest => {
   let args: pb::auth::TestArgs = args_decode(args,"auth::Test")?;
   let headers = &ctx.req.headers;
-  match auth::test(args.timezone as i8,args.dpi as u8,args.w as u16,args.h as u16,&args.os_ver,&args.arch,args.cpu_num,&args.gpu,&headers).await {
-    Err(err)=>call_err("auth::test", err, captcha, || dvec![args.timezone as i8,args.dpi as u8,args.w as u16,args.h as u16,&args.os_ver,&args.arch,args.cpu_num,&args.gpu,&headers].join(",")).await?,
-    Ok(_)=>(State::OK, vec![])
-  }
+auth::test(args.timezone.0 as i8,args.dpi as u8,args.w as u16,args.h as u16,&args.os_ver,&args.arch,args.cpu_num,&args.gpu,&headers);
+(State::OK, vec![])
 }
-
 
 Func::DemoCaptcha => {
   ctx_::captcha(ctx,captcha).await?;
   let headers = &ctx.req.headers;
   match demo::captcha(&headers).await {
-    Err(err)=>call_err("demo::captcha", err, captcha, || dvec![&headers].join(",")).await?,
+Err(err)=>call_err("demo::captcha", err, captcha, || dvec![&headers].join(",")).await?,
     Ok(_)=>(State::OK, vec![])
   }
 }
@@ -99,7 +95,7 @@ Func::DemoCaptcha => {
 Func::DemoManualCaptcha => {
   let headers = &ctx.req.headers;
   match demo::manual_captcha(&headers).await {
-    Err(err)=>call_err("demo::manual_captcha", err, captcha, || dvec![&headers].join(",")).await?,
+Err(err)=>call_err("demo::manual_captcha", err, captcha, || dvec![&headers].join(",")).await?,
     Ok(_)=>(State::OK, vec![])
   }
 }
